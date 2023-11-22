7369cf5ed23e158b12ff941aa5000ce4ca7936a1
kangyuri1114 <yuri3334@gmail.com>
2023-11-22 18:20:08 +0900

    [ADD] : ExploreImage 객체 데이터 클래스 추가 / #4

---
+++ app/src/main/java/com/example/airbnb/data/ExploreImage.kt
@@ -0,0 +1,10 @@
+package com.example.airbnb.data
+
+data class ExploreImage(
+    val Image: Int,
+    val title: String,
+    val location: String,
+    val price: String,
+    val score: String,
+    var favorite: Boolean,
+)
