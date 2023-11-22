e713c71f17933ddb7e53bdaac34e6e0273ab48ed
kangyuri1114 <yuri3334@gmail.com>
2023-11-22 19:06:06 +0900

    [MOD] : ExploreImage 객체 데이터 클래스 추가 바탕으로 어댑터 수정 / #4

---
+++ app/src/main/java/com/example/airbnb/data/DummyExploreImageList.kt
@@ -0,0 +1,41 @@
+package com.example.airbnb.data
+
+import com.example.airbnb.R
+
+object DummyExploreImageList {
+
+    val dummyExploreImage = listOf(
+        ExploreImage(
+            Image = R.drawable.ex_good1,
+            title = "ex_good1",
+            location = "ex_good1",
+            price = "ex_good1",
+            score = "4.56",
+            favorite = true,
+        ),
+        ExploreImage(
+            Image = R.drawable.ex_good2,
+            title = "ex_good2",
+            location = "ex_good2",
+            price = "ex_good2",
+            score = "1",
+            favorite = true,
+        ),
+        ExploreImage(
+            Image = R.drawable.ex_room1,
+            title = "ex_room1",
+            location = "ex_room1",
+            price = "ex_room1",
+            score = "4.56",
+            favorite = false,
+        ),
+        ExploreImage(
+            Image = R.drawable.ex_room2,
+            title = "ex_good1",
+            location = "ex_good1",
+            price = "ex_good1",
+            score = "4.56",
+            favorite = true,
+        ),
+    )
+}
--- app/src/main/java/com/example/airbnb/presentation/explore/ExploreViewPagerAdapter.kt
+++ app/src/main/java/com/example/airbnb/presentation/explore/ExploreViewPagerAdapter.kt
@@ -2,23 +2,40 @@ package com.example.airbnb.presentation.explore
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.example.airbnb.R
+import com.example.airbnb.data.ExploreImage
+import com.example.airbnb.databinding.ItemExploreViewpageBinding
 
-class ExploreViewPagerAdapter(var ImageList: ArrayList<Int>) :
+class ExploreViewPagerAdapter(private val imageList: List<ExploreImage>) :
     RecyclerView.Adapter<ExploreViewPagerAdapter.PagerViewHolder>() {
 
-    inner class PagerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder
-        (LayoutInflater.from(parent.context).inflate(R.layout.item_explore_viewpage, parent, false)) {
-        val ImageList = itemView.findViewById<ImageView>(R.id.item_viewPager_Image)
+    // private var imageList: List<ExploreImage> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagerViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_explore_viewpage, parent, false)
+        return PagerViewHolder(ItemExploreViewpageBinding.bind(view))
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PagerViewHolder((parent))
+    class PagerViewHolder(private val binding: ItemExploreViewpageBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-    override fun getItemCount(): Int = ImageList.size
+        fun onBindView(imageInfo: ExploreImage) {
+            binding.run {
+                itemViewPagerImage.load(imageInfo.Image) {
+                    placeholder(R.drawable.shape_gray_fill_14_rect_image_loading)
+                    error(R.drawable.shape_gray_fill_14_rect_image_loading)
+                }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = imageList.size
 
     override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
-        holder.ImageList.setImageResource(ImageList[position])
+        val exploreImage = imageList[position]
+        holder.onBindView(exploreImage)
     }
 }
