7ad1e05ee8bc4e22f1a1e409b8737c284341d20f
kangyuri1114 <yuri3334@gmail.com>
2023-11-23 23:59:49 +0900

    [MOD] : 화면 이동 코드 추가 / #4

--- app/src/main/java/com/example/airbnb/presentation/explore/ExploreFragment.kt
+++ app/src/main/java/com/example/airbnb/presentation/explore/ExploreFragment.kt
@@ -1,9 +1,11 @@
 package com.example.airbnb.presentation.explore
 
+import android.content.Intent
 import com.example.airbnb.R
 import com.example.airbnb.core.base.BindingFragment
 import com.example.airbnb.data.DummyExploreImageList
 import com.example.airbnb.databinding.FragmentExploreBinding
+import com.example.airbnb.presentation.where.WhereActivity
 
 class ExploreFragment : BindingFragment<FragmentExploreBinding>(R.layout.fragment_explore) {
 
@@ -13,9 +15,22 @@ class ExploreFragment : BindingFragment<FragmentExploreBinding>(R.layout.fragmen
         // 로직 작성
         val dummyExploreImageList = DummyExploreImageList.dummyExploreImage
         exploreViewPagerAdapter = ExploreViewPagerAdapter(dummyExploreImageList)
-        binding.vpExplore.adapter = exploreViewPagerAdapter
-        binding.indicatorViewpagerImageDots.attachTo(binding.vpExplore)
-        binding.vpExplore.offscreenPageLimit = 3
-        binding.vpExplore.setPadding(40, 0, 40, 0)
+
+        binding.run {
+            // 뷰페이저 어댑터 연결
+            vpExplore.adapter = exploreViewPagerAdapter
+            indicatorViewpagerImageDots.attachTo(binding.vpExplore)
+
+            // 뷰페이저 화면 디자인
+            vpExplore.offscreenPageLimit = 3
+            vpExplore.setPadding(40, 0, 40, 0)
+
+            // 화면 이동
+            ivMainSearchBar.setOnClickListener {
+                val intent = Intent(requireContext(), WhereActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                startActivity(intent)
+            }
+        }
     }
 }
