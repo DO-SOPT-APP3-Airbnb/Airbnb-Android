91418a9a1a46891be1a688e8d1735ad6b06aab6b
Eonji-sw <ejkim0625@gmail.com>
2023-11-29 20:31:56 +0900

    [REFACTOR] : 코드 리팩토링

--- app/src/main/java/com/example/airbnb/presentation/who/WhoViewModel.kt
+++ app/src/main/java/com/example/airbnb/presentation/who/WhoViewModel.kt
@@ -2,17 +2,19 @@ package com.example.airbnb.presentation.who
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import java.lang.Integer.max
+import java.util.concurrent.ConcurrentHashMap
 
 class WhoViewModel : ViewModel() {
 
-    private val whoCountMap: MutableMap<String, MutableLiveData<Int>> = HashMap()
+    private val whoCountMap: MutableMap<String, MutableLiveData<Int>> = ConcurrentHashMap()
+    private val types = arrayOf("adult", "kids", "toddler", "pet")
 
     init {
-        whoCountMap["adult"] = MutableLiveData(0)
-        whoCountMap["kids"] = MutableLiveData(0)
-        whoCountMap["toddler"] = MutableLiveData(0)
-        whoCountMap["pet"] = MutableLiveData(0)
+        types.forEach { initCount(it) }
+    }
+
+    private fun initCount(type: String) {
+        whoCountMap[type] = MutableLiveData(0)
     }
 
     fun getCount(type: String): MutableLiveData<Int> {
@@ -24,6 +26,6 @@ class WhoViewModel : ViewModel() {
     }
 
     fun decreaseCount(type: String) {
-        getCount(type).value = max(0, getCount(type).value?.minus(1) ?: 0)
+        getCount(type).value = maxOf(0, getCount(type).value?.minus(1) ?: 0)
     }
 }
