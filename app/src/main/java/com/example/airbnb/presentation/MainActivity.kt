892e25e3d40a10f4eb7901aa95a4e65b8d945c6b
Sohyun <so405@naver.com>
2023-11-30 18:53:51 +0900

    [FIX] : main activity 예제 코드 제거 / #14

--- app/src/main/java/com/example/airbnb/presentation/MainActivity.kt
+++ app/src/main/java/com/example/airbnb/presentation/MainActivity.kt
@@ -11,11 +11,8 @@ import timber.log.Timber
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    private val exampleViewModel by viewModels<ExampleViewModel>()
-
     override fun initView() {
         // 로직 작성
-        // exampleObserve()
         initialFragment(R.id.fcv_main, ExploreFragment())
     }
 
@@ -27,16 +24,6 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
             supportFragmentManager.beginTransaction()
                 .add(containerViewId, fragment)
                 .commit()
-        }
-    }
-
-    private fun exampleObserve() {
-        exampleViewModel.exampleLiveData.observe(this) {
-            when (it) {
-                is UiState.Success -> Timber.d("성공")
-                is UiState.Failure -> Timber.d("실패")
-                is UiState.Loading -> Timber.d("로딩중")
-            }
         }
     }
 }
